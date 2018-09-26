31339792e758b88738bc002b37c6f412464748ee
Corey Cunningham <coreycunningham@Roberts-Mini.fios-router.home>
2018-09-25 22:07:57 -0400

    parses index.xml now must parse  rooms

--- src/controller/consumer/parse_xml.ts
+++ src/controller/consumer/parse_xml.ts
@@ -1,6 +1,7 @@
 import * as parse5 from "parse5";
 import * as JSzip from "jszip";
 import Log from "../../Util";
+import { inspect } from "util";
 
 export default class XMLParse {
 
@@ -34,14 +35,39 @@ export default class XMLParse {
     }
 
     public parse(xml: string): object[] {
-        // const document: { } = parse5.parse(xml);
-        // for (const node of document.childNodes) {
-        //     // TODO
-        // }
-        return [{}];
+        const doc: { [childNodes: string]: any } = parse5.parse(xml);
+        // GET to the BUILDINGS
+        const html: any = doc.childNodes[1];
+        const body: any = html.childNodes[1];
+        const container: any = body.childNodes[0];
+        // Filter everything thats not a building (text)
+        let buildings: any = container.childNodes.filter((x: any) => x.tagName === "building" );
+        buildings = buildings.map( (building: any) => {
+            const x: any = {};
+            // loop through attributes of building tag and make object with name, value pairs
+            for (const buildingAttr of building.attrs) {
+                x[buildingAttr.name] = buildingAttr.value;
+            }
+            // loop through attributes of location tag and make object with name, value pairs
+            const location: any = building.childNodes.filter( (node: any) => node.tagName === "location");
+            const locAttrs: any = location[0].attrs;
+            for (const locAttr of locAttrs) {
+                x[locAttr.name] = locAttr.value;
+            }
+            return x;
+        });
+        return buildings;
     }
 
-    private getBuilding() {
-        // TODO
+    private async parseRooms(path: string): Promise<object[]> {
+        const file: JSzip.JSZipObject = this.jszip.file(path);
+        const xml: string = await file.async("text");
+        const doc: any = parse5.parse(xml);
+        const html: any = doc.childNodes[1];
+        const body: any = html.childNodes[1];
+        const building: any = body.childNodes[0];
+        let rooms: any = building.childNodes[0];
+        rooms = rooms.filter( (room: any) => room.tagName === "room" );
+        return Promise.resolve([{}]);
     }
 }
--- test/XMLparse.spec.ts
+++ test/XMLparse.spec.ts
@@ -1,6 +1,6 @@
 import XMLParse from "../src/controller/consumer/parse_xml";
 import * as fs from "fs";
-import { promisify, inspect } from "util";
+import { promisify, inspect, isArray } from "util";
 import { JSZipObject } from "../node_modules/@types/jszip";
 import { expect } from "chai";
 import * as path from "path";
@@ -28,25 +28,8 @@ describe("XMLPARSE", () => {
     it("Should return true with a valid XML document", async () => {
         const index: JSZipObject = await parser.getIndex();
         const xml: string = await index.async("text");
-        const doc: { [childNodes: string]: any } = parse5.parse(xml);
-        let buildings: any = doc["childNodes"][1]["childNodes"][1]["childNodes"][0]["childNodes"];
-        const data: object[] = [];
+        const buildings: any = parser.parse(xml);
         Log.test(inspect(buildings));
-        buildings = buildings.filter( (x: any) => x["nodeName"] === "building" );
-        for (const building of buildings) {
-            Log.test("BUILDING");
-            const makeBuild: any = {};
-            for (const attr of buildings["attrs"]) {
-                makeBuild[attr["name"]] = attr["value"];
-            }
-            const location: any = building["childNodes"].filter( (x: any) => x["nodeName"] === "location");
-            for (const attr of location["attrs"]) {
-                makeBuild[attr["name"]] = attr["value"];
-            }
-            Log.test(JSON.stringify(makeBuild));
-            data.push(makeBuild);
-        }
-        Log.test(JSON.stringify(data));
     });
 
     it("Should return false if given invalid XML document", () => {
