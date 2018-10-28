2d239fec38436f1c57269c37b7b0ab9ce8dc2870
Corey Cunningham <coreycunningham@Roberts-Mini.fios-router.home>
2018-10-27 21:21:33 -0400

    no more js files

--- src/controller/IInsightFacade.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var InsightDatasetKind;
-(function (InsightDatasetKind) {
-    InsightDatasetKind["Courses"] = "courses";
-    InsightDatasetKind["Rooms"] = "rooms";
-})(InsightDatasetKind = exports.InsightDatasetKind || (exports.InsightDatasetKind = {}));
-//# sourceMappingURL=IInsightFacade.js.map
